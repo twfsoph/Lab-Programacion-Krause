a6d182bcd7c8faa79e566928610759ac62d5ce93
twfsoph <sophiauribeok@gmail.com>
2026-04-27 08:53:38 -0300

    si

--- demo/src/main/java/com/trabajopractico/tp3/Ejercicio2/Main.java
+++ demo/src/main/java/com/trabajopractico/tp3/Ejercicio2/Main.java
@@ -22,3 +22,46 @@ public class Main {
         tri.agregarLibro(j);
     }
 }
+
+class Socio {
+    String nombre;
+    int id;
+
+    Libro libro1;
+    Libro libro2;
+    Libro libro3;
+    Libro libro4;
+    Libro libro5;
+
+    int contador = 0;
+
+    public Socio(String nombre, int id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public void agregarLibro(Libro libro) {
+        if (contador == 0) {
+            libro1 = libro;
+        } else if (contador == 1) {
+            libro2 = libro;
+        } else if (contador == 2) {
+            libro3 = libro;
+        } else if (contador == 3) {
+            libro4 = libro;
+        } else if (contador == 4) {
+            libro5 = libro;
+        }
+        contador++;
+    }
+}
+
+class Libro {
+    String id;
+    String nombre;
+
+    public Libro(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+}
